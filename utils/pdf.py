9dfa7184ced7ba33ed868c57c5917473acd5dc8f
Emmanuel Bouthenot <kolter@openics.org>
2009-07-05 22:18:57 +0200

    Add an ugly workaround against trml2pdf with utf-8 encoding

--- utils/pdf.py
+++ utils/pdf.py
@@ -9,4 +9,10 @@ def gen_pdf(tmpl, ctx):
     if len(settings.TEMPLATE_DIRS) > 0:
         ctx['img_path'] = "%s/images/" % (settings.TEMPLATE_DIRS[0])
     t = loader.get_template(tmpl)
-    return trml2pdf.parseString(t.render(Context(ctx)).encode('utf-8'))
+    buffer = t.render(Context(ctx)).encode('utf-8')
+    # no comment
+    buffer = buffer.replace('è', 'e')
+    buffer = buffer.replace('ô', 'o')
+    buffer = buffer.replace('ä', 'a')
+    buffer = buffer.replace('ü', 'u')
+    return trml2pdf.parseString(buffer)
