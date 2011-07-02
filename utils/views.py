65e6983ee5826c09ddadfd2ec65f3d32f445be19
Emmanuel Bouthenot <kolter@openics.org>
2011-07-02 20:53:52 +0200

    Rewrite CSV export for users accounts to use csv builtin module

--- utils/views.py
+++ utils/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import csv, time
+
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 
@@ -8,11 +10,19 @@ from resarmll.utils.decorators import http_basicauth
 
 @http_basicauth(settings.global_httpauth_username, settings.global_httpauth_password)
 def sync(request):
-    content = ''
-    users = User.objects.all()
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachement; filename=rmll_accounts_%s.csv' % (time.strftime('%Y%m%d-%H%M%S',  time.localtime()))
+    writer = csv.writer(response, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
+    users = User.objects.all().order_by('username')
     if users:
         for u in users:
-            datas = u.password.split('$')
-            content += '"%s";"%s";"%s";"%s"\n' % (u.username, datas[1], datas[2], int(u.is_active))
-    return HttpResponse(content, mimetype="text/html")
+            password_parts = u.password.split('$')
+            fields = []
+            fields.append(u.username.encode('utf-8'))
+            fields.append(password_parts[1])
+            fields.append(password_parts[2])
+            fields.append(int(u.is_active))
+            fields.append(u.email)
+            writer.writerow(fields)
+    return response
 
